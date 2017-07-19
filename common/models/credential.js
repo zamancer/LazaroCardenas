e6ff35e160443bbd68115920be7561ee85ddce5c
Alan Zambrano <alan.zambrano@globant.com>
2017-07-18 21:31:44 -0500

    Added /Credential/register endpoint

--- common/models/credential.js
+++ common/models/credential.js
@@ -1,5 +1,41 @@
-'use strict';
-
 module.exports = function(Credential) {
+  const selectUserBuilder = function(userAccount) {
+    switch (userAccount.userType) {
+    //   case '1':
+    //     return {
+    //       owner: {ownerType: 'CulturalHelper'},
+    //     };
+      case '2':
+        return {
+          owner: {ownerType: 'Artist'},
+          model: Credential.app.models.Artist,
+        };
+      default:
+        throw new Error('Unsupported user type');
+    }
+  };
 
+ /**
+ * Registers a new user Credential
+ * @param {object} userAccount The details for the new user
+ * @param {Function(Error, object)} callback
+ */
+  Credential.register = function(userAccount, callback) {
+    let userTypeBuilder = selectUserBuilder(userAccount);
+    const Model = userTypeBuilder.model;
+
+    return Promise.resolve()
+        .then(() => Model.create({}))
+        .then((ownerModel) => {
+          let newCredential = Object.assign({},
+                                userAccount,
+                                userTypeBuilder.owner,
+                                {ownerId: ownerModel.id});
+
+          return Credential.create(newCredential);
+        })
+        .then(credential => {
+          return credential;
+        });
+  };
 };
